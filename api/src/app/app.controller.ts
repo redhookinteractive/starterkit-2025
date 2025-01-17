8cf2fa0c1c751a05de4ec8faebf0f584f6e44bfd
Dustin Rea <drea08@gmail.com>
2025-01-17 18:08:11 -0500

    adds prisma and postgres

--- api/src/app/app.controller.ts
+++ api/src/app/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { PrismaService } from '../prisma/prisma.service';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService
+  ) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('/users')
+  async getUsers() {
+    return this.prisma.user.findMany();
   }
 }
